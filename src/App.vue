8015e98efbc6b400be7f9f18154e329d09dca611
Bastien <bastien.nicoud@gmail.com>
2017-12-05 16:00:04 +0100

    initial project

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Service Catalog
+          </h1>
+          <h2 class="subtitle">
+            cpnv
+          </h2>
+        </div>
+      </div>
+    </section>
+
+    <router-view/>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Service Catalog</strong> by <a href="https://github.com/bastiennicoud">Bastien Nicoud</a>
+          </p>
+        </div>
+      </div>
+    </footer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+}
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
